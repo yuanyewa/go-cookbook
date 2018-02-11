8b8e8ecf02367a1574c5b089a0947fa0087d99b5
yuanyewa <afakeid@yahoo.com>
2018-02-10 20:48:43 -0800

    add my file

---
+++ chapter1/filedirs/example/myMain.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if err := os.Mkdir("example_dir", os.FileMode(0755)); err != nil {
+		fmt.Println(err)
+	}
+	if err := os.Chdir("example_dir"); err != nil {
+		fmt.Println(err)
+	}
+	f, err := os.Create("aFile.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	value := []byte("this is the content")
+	f.Write(value)
+	f.Close()
+
+	f, _ = os.Open("aFile.txt")
+	io.Copy(os.Stdout, f)
+	f.Close()
+
+}
